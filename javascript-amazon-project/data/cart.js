4f1448db357f5ece8c09015c8206563e9bc448da
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-18 14:21:03 +0900

    save cart to local storage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,14 +1,23 @@
 // export lets the var be accessed outside this script, with 'module'
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) { // if there is no cart saved in local storage, set a default value
+  cart =
+  [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    }
+  ];
+}
+// save data locally
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart)); // local storage can only save strings, so must be converted
+}
 
 export function addToCart(selectedProductId) {
   let matchingItem;
@@ -28,6 +37,8 @@ export function addToCart(selectedProductId) {
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(selectedProductId) {
@@ -43,4 +54,6 @@ export function removeFromCart(selectedProductId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
