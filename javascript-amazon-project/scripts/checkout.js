a85c009e4469dee36dd38198f3d1a5a4c34370b3
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-18 15:48:09 +0900

    save data and generate html for delivery options

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -2,6 +2,19 @@
 import { cart, removeFromCart } from '../data/cart.js'
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+// a default export, used when you only want to export one thing from a file. doesn't use {}. libraries which use {}are 'named' exports
+import  dayjs  from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { deliveryOptions } from '../data/deliveryOptions.js';
+
+
+hello();
+
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'days'); // add 7 days to current date
+
+console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHTML = '';
 
@@ -17,11 +30,25 @@ cart.forEach((cartItem) => {
     }
   });
 
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+  const dateString = deliveryDate.format('dddd, MMMM D');
+
   cartSummaryHTML += `
       <div class="cart-item-container 
       js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -53,45 +80,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHTML(matchingProduct, cartItem)}
         </div>
       </div>
     </div>
@@ -99,7 +88,63 @@ cart.forEach((cartItem) => {
 });
 
 
+function deliveryOptionsHTML(matchingProduct, cartItem) {
+  //1.loop through the delivery options
+  //2.for each option generate html
+  //3.combine the html together
+
+  let html = '';
+
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    const dateString = deliveryDate.format('dddd, MMMM D');
+
+    const priceString = deliveryOption.priceCents === 0 ? 'FREE': 
+      `$${formatCurrency(deliveryOption.priceCents)}`;
+    
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+    // possible to insert turnary operator into html element
+    // ${isChecked ? 'checked': ''} // checked is an attribute of radio button
+    html += 
+    `
+    <div class="delivery-option">
+      <input type="radio"
+        ${isChecked ? 'checked': ''} 
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
+      <div>
+        <div class="delivery-option-date">
+          ${dateString}
+        </div>
+        <div class="delivery-option-price">
+          ${priceString} - Shipping
+        </div>
+      </div>
+    </div>
+    `
+  });
+
+  return html;
+}
+
+
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+
+function updateCheckoutHeader() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-checkout-header').innerHTML = 
+`${cartQuantity} items`;
+}
+
+updateCheckoutHeader();
 
 
 document.querySelectorAll('.js-delete-link')
@@ -112,5 +157,7 @@ document.querySelectorAll('.js-delete-link')
       const container = document.querySelector(`.js-cart-item-container-${selectedProductID}`);
 
       container.remove();
+      updateCheckoutHeader();
     });
+
 });
--- javascript-amazon-project/scripts/utils/money.js
+++ javascript-amazon-project/scripts/utils/money.js
@@ -2,3 +2,7 @@
 export function formatCurrency(priceCents) {
   return (priceCents / 100).toFixed(2);
 }
+
+// default export is used to export only 1 thing
+// each file can only have 1 default export
+export default formatCurrency;
