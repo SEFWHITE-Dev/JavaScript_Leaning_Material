8b407523058e737e31fef2d1fc727d617f33f2a2
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-18 14:15:24 +0900

    delete products from HTML when clicking delete

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-      <div class="cart-item-container">
+      <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -107,7 +108,9 @@ document.querySelectorAll('.js-delete-link')
       const selectedProductID = link.dataset.productId;
       
       removeFromCart(selectedProductID);
-      
-      console.log(cart);
+
+      const container = document.querySelector(`.js-cart-item-container-${selectedProductID}`);
+
+      container.remove();
     });
 });
