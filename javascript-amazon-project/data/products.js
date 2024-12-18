bf28b3e21f52c56894046845b2e81dcd21ac87e3
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-18 17:23:50 +0900

    save data and generate html ofr payment summary

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -1,3 +1,19 @@
+
+
+
+export function getProduct(productId) {
+  let matchingProduct;
+    
+  products.forEach((product) => { // de-duplicate/normalize the data, by creating a local copy of it
+    if (productId === product.id) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -1,11 +1,11 @@
 
 import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js'
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 // a default export, used when you only want to export one thing from a file. doesn't use {}. libraries which use {}are 'named' exports
 import  dayjs  from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 
 
 hello();
@@ -31,23 +31,11 @@ export function renderOrderSummary() {
 
     const productId = cartItem.productId;
 
-    let matchingProduct;
-    
-    products.forEach((product) => { // de-duplicate/normalize the data, by creating a local copy of it
-      if (productId === product.id) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
