f04be2c1222c991ac1aaec231e8f096e7983788c
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-18 11:33:21 +0900

    make the cart quantity interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -124,10 +124,18 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
     }
     else {
       cart.push({
-        productName: selectedProductId,
+        productId: selectedProductId,
         quantity: 1
       });
     }
-    console.log(cart);
-  })
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+  });
 });
