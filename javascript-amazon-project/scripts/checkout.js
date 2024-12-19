c37225723792d854d6341cb77bad34b818623195
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-19 17:34:03 +0900

    create oop version of cart

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js'; // this syntx just runs the code within the file without importing anything
 
 renderOrderSummary();
 renderPaymentSummary();
