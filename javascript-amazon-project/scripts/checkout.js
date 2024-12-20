b9fd7a12f7e5757e0997a772990b1e6a9f561189
WhiteNoise01 <s.e.f.white.ads@gmail.com>
2024-12-20 14:44:26 +0900

    add backend practice file

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -2,5 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-oop.js'; // this syntx just runs the code within the file without importing anything
 
+import '../data/backend-practice.js'
+
 renderOrderSummary();
 renderPaymentSummary();
